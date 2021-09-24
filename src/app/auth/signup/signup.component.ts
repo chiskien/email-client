0219e9c086b10e2ec59c417a14bb884c366ec412
ChisKien <chiskien03214@gmail.com>
2021-09-24 20:08:05 +0700

    finish validators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
-import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Component, OnInit} from '@angular/core';
-import {Match} from "../validators/match";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatchPassword} from '../validators/match-password';
+import {UniqueUsername} from '../validators/unique-username';
 
 @Component({
   selector: 'app-signup',
@@ -8,29 +9,42 @@ import {Match} from "../validators/match";
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  authForm = new FormGroup({
-    username: new FormControl('CK', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(20),
-      Validators.pattern(/^[a-z0-9]+$/)
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(20),
-    ]),
-    passwordConfirmation: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4),
-      Validators.maxLength(20),
-    ]),
-  }, {validators: [this.match.validate]});
-
-  constructor(private match: Match) {
+  authForm = new FormGroup(
+    {
+      username: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/)
+        ],
+        [this.uniqueUsername.validate]
+      ),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20)
+      ]),
+      passwordConfirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20)
+      ])
+    },
+    {validators: [this.matchPassword.validate]}
+  );
+  constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername
+  ) {
   }
-
-  ngOnInit(): void {
+  onSubmit() {
+    if(this.authForm.invalid) {
+      return;
+    }
+    console.log(this.authForm.value);
   }
-
+  ngOnInit() {
+  }
 }
